32c82a29bf3b6b8b7513d6eabf1ae25bb9cde963
kunsangabo ndongala <berfy@simpleandnew.com>
2019-08-30 12:30:37 +0200

    template of elearning project

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,61 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require jquery-2.2.4.min
+//= require jquery-ui-1.8.22.min
+//= require jquery-ui.custom.min
+//= require admission_func
+//= require bootstrap
+//= require bootstrap.min
+
+//= require classie
+//= require common_scripts
+//= require moment.min
+//= require fullcalendar_func
+//= require fullcalendar
+//= require fullcalendar.min
+//= require icheck
+//= require icheck.min
+
+//= require jquery.cookiebar
+//= require jquery.flexslider
+//= require jquery.flexslider.uncompressed
+//= require jquery.magnific-popup
+//= require jquery.magnific-popup.min
+//= require jquery.selectbox-0.2
+//= require jquery.validate
+//= require jquery.wizard
+//= require main
+
+//= require mapmarker_func.jquery
+//= require mapmarker.jquery
+//= require modernizr_slider
+//= require modernizr_tables
+//= require modernizr
+
+//= require owl.carousel
+
+//= require owl.carousel.min
+//= require popper.min
+//= require pw_strenght
+//= require ResizeSensor
+//= require ResizeSensor.min
+//= require retina-replace
+//= require retina-replace.min.js
+//= require sticky-kit-1.4
+//= require sticky-kit-1.4.min
+//= require sticky-kit
+//= require sticky-kit.min
+//= require_tree .
